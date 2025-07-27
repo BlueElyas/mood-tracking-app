e135bd26fbf0892fd6e6c70bc13dd71d82771373
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-07-27 12:56:47 +0100

    update input component

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,4 +1,4 @@
-import { TextField, Label, Input } from "react-aria-components";
+import { Input } from "react-aria-components";
 
 interface Props {
   type: string;
@@ -12,21 +12,18 @@ interface Props {
 function TextInput(props: Props) {
   const { type, name, isDisabled, isLoading, isRequired, placeholder } = props;
 
-  const className = `max-w-[466px] borer-radius fill-neutral-0 text-neutral-600 border-neutral-300 border-2 border-solid rounded-md`;
+  const className = `max-w-[466px] borer-radius fill-neutral-0 text-neutral-600 border-neutral-300 border-2 border-solid rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:ring-offset-white`;
 
   return (
-    <TextField className="flex flex-col gap-2">
-      <Label>{name}</Label>
-      <Input
-        type={type}
-        required={isRequired}
-        disabled={isDisabled}
-        name={name}
-        aria-busy={isLoading}
-        className={className}
-        placeholder={placeholder}
-      />
-    </TextField>
+    <Input
+      type={type}
+      required={isRequired}
+      disabled={isDisabled}
+      name={name}
+      aria-busy={isLoading}
+      className={className}
+      placeholder={placeholder}
+    />
   );
 }
 
