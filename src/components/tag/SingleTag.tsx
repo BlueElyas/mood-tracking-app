f9155b7f2ce4e10f0a514d57a76327dc15049368
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-07-27 13:29:31 +0100

    adjust

--- src/components/tag/SingleTag.tsx
+++ src/components/tag/SingleTag.tsx
@@ -9,7 +9,7 @@ function SingleTag({ value }: Props) {
     <Radio
       value={value}
       className={({ isSelected, isFocusVisible }) =>
-        `group flex items-center gap-2 px-3 py-2  w-[176px] rounded-md border-2 ${
+        `group flex items-center gap-2 px-3 py-2 rounded-md border-2 ${
           isSelected ? "bg-blue-100 border-blue-500" : "border-blue-100"
         } ${
           isFocusVisible
