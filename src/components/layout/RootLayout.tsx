0671d85e08cdbcdfa5acdf39463eee5cb1f4e0d9
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-07-28 18:19:44 +0100

    adjust naming

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function RootLayout() {
   return (
     <>
       <div className="bg-lightGradient">
-        <Header />
+        <Navbar />
         <Outlet />
       </div>
     </>
