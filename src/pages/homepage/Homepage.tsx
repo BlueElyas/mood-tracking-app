f396b37d5167456a1b4cc2e26fa10c7a1981c1bb
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-07-28 19:05:35 +0100

    Replicate homepage design more

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,10 +1,19 @@
+import Container from "../../components/container/Container";
+import AverageBox from "./components/AverageBox";
 import Hero from "./components/Hero";
 
 function Homepage() {
   return (
-    <>
+    <div className="flex flex-col justify-center  p-4">
       <Hero />
-    </>
+      <div className="flex  justify-center gap-6 mt-8">
+        <Container>
+          <AverageBox title="Mood" />
+          <AverageBox title="Sleep" />
+        </Container>
+        <Container>Graph here</Container>
+      </div>
+    </div>
   );
 }
 
--- src/pages/homepage/components/Hero.tsx
+++ src/pages/homepage/components/Hero.tsx
@@ -5,10 +5,12 @@ function Hero() {
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-4 p-8  ">
-        <h2 className="text-blue-900 font-bold">Hello, Lisa!</h2>
-        <div className="text-center">
-          <h1 className="text-3xl font-bold">How are you feeling today?</h1>
-          <p className="text-neutral-600 text-sm mt-3">{getFormattedDate()}</p>
+        <h2 className="text-blue-900 text-xl font-bold">Hello, Lisa!</h2>
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-bold">How are you feeling today?</h1>
+          <p className="text-neutral-600 text-sm mt-3 font-semibold">
+            {getFormattedDate()}
+          </p>
         </div>
         <MoodLoggerModal />
       </div>
