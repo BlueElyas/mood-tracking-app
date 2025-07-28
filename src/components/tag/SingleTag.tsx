6022b4aaf90cecc5c0428e96c17d5ae2ef1faa2f
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-07-28 17:50:25 +0100

    style updates on singletag

--- src/components/tag/SingleTag.tsx
+++ src/components/tag/SingleTag.tsx
@@ -14,8 +14,8 @@ function SingleTag(props: Props) {
     <Radio
       value={value}
       className={({ isSelected, isFocusVisible }) =>
-        `group flex items-center gap-2 px-3 py-2 rounded-md border-2 justify-between  ${
-          isSelected ? "bg-blue-100 border-blue-500" : "border-blue-100"
+        `group flex items-center gap-2 px-3 py-3 rounded-md border-2 justify-between  ${
+          isSelected ? "bg-blue-200 border-blue-500" : "border-blue-200"
         } ${
           isFocusVisible
             ? "ring-2 ring-blue-500 ring-offset-2 ring-offset-white"
