a4aea3180605ab8557650c8f77481ddef65c2103
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-07-29 21:33:04 +0100

    Create barchartmoods and constant with data

---
+++ src/components/chart/constants.ts
@@ -0,0 +1,22 @@
+export const barStyle = [
+  {
+    emoji: "very-sad-color",
+    fill: "bg-red-300",
+  },
+  {
+    emoji: "sad-color",
+    fill: "bg-indigo-200",
+  },
+  {
+    emoji: "neutral-color",
+    fill: "bg-blue-300",
+  },
+  {
+    emoji: "happy-color",
+    fill: "bg-green-300",
+  },
+  {
+    emoji: "very-happy-color",
+    fill: "bg-amber-300",
+  },
+];
