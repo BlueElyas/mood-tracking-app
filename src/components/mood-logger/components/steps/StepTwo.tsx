5f171720da955ac11d2af54f8cf5c6a74efc5e51
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-07-29 22:35:15 +0100

    Add ability to save for step two

--- src/components/mood-logger/components/steps/StepTwo.tsx
+++ src/components/mood-logger/components/steps/StepTwo.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import BasicButton from "../../../button/BasicButton";
 import MultipleTag from "../../../tag/MultipleTag";
 import { feelingTags } from "../../constants";
 import ProgressBar from "../ProgressBar";
-import { RadioGroup, Text } from "react-aria-components";
+import { Text } from "react-aria-components";
 
 type StepProps = {
   data: any;
@@ -12,8 +13,25 @@ type StepProps = {
 };
 
 function StepTwo(props: StepProps) {
-  const { next, data } = props;
-  console.log(data);
+  const { next, data, updateForm } = props;
+  const [feelings, setFeelings] = useState<string[]>(data?.feelings || []);
+
+  // Allow any number of selections
+  const handleChange = (value: string, checked: boolean) => {
+    if (checked) {
+      setFeelings([...feelings, value]);
+    } else {
+      setFeelings(feelings.filter((f) => f !== value));
+    }
+  };
+
+  const handleContinue = () => {
+    updateForm("feelings", feelings);
+    next();
+  };
+
+  const isDisabled = feelings.length === 0 || feelings.length > 3;
+
   return (
     <div className="space-y-6">
       <ProgressBar isStepTwo />
@@ -22,14 +40,25 @@ function StepTwo(props: StepProps) {
           How did you feel?
         </Text>
         <Text>Select up to three tags:</Text>
-        <RadioGroup className="flex flex-wrap gap-4">
-          {feelingTags.map((tag) => {
-            return <MultipleTag key={tag} value={tag} />;
-          })}
-        </RadioGroup>
+        <div className="flex flex-wrap gap-4">
+          {feelingTags.map((tag) => (
+            <MultipleTag
+              key={tag}
+              value={tag}
+              isSelected={feelings.includes(tag)}
+              onChange={(checked: boolean) => handleChange(tag, checked)}
+            />
+          ))}
+        </div>
       </div>
-      <BasicButton style="w-full" onPress={next}>
-        Continue
+      <BasicButton
+        style={`w-full ${
+          isDisabled ? "bg-red-300 hover:bg-red-300 cursor-not-allowed" : ""
+        }`}
+        onPress={handleContinue}
+        disabled={isDisabled}
+      >
+        {isDisabled ? "You can only select upto 3" : "Continue"}
       </BasicButton>
     </div>
   );
