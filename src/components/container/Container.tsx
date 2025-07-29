5549159e79542cb3835aab787da412e12b052c32
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-07-29 21:32:21 +0100

    Update component to be more flexibile

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -2,13 +2,16 @@ import type { PropsWithChildren, ReactNode } from "react";
 
 interface Props extends PropsWithChildren {
   children: ReactNode;
+  className?: string;
 }
 
 function Container(props: Props) {
-  const { children } = props;
+  const { children, className } = props;
 
   return (
-    <div className="bg-white rounded-2xl p-6 shadow-md flex flex-col items-start justify-center min-h-[140px] w-full max-w-xs gap-4 ">
+    <div
+      className={`bg-white rounded-2xl p-6 shadow-md flex flex-col items-start justify-center min-h-[140px] w-full max-w-xs gap-4 ${className}`}
+    >
       {children}
     </div>
   );
