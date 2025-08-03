7947e5c570cfd5e7d602fd0d4dff74216a0acb3a
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-08-03 18:04:11 +0100

    Update login and create account pages

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,15 +1,87 @@
-import AuthForm from "../../components/authform/AuthForm";
+import Icon from "../../components/icon/Icon";
+import { Formik } from "formik";
+import { Label, Text } from "react-aria-components";
+import BasicButton from "../../components/button/BasicButton";
+import TextInput from "../../components/input/TextInput";
+import * as Yup from "yup";
 
 function Login() {
+  const title = "Welcome back!";
+  const helperText = "Log in to continue tracking your mood and sleep";
+  const buttonText = "Sign In";
+  const linkText = "Don't have an account?";
+  const linkHref = "/create-account";
+  const linkAction = "Create an account";
+
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email")
+      .min(2, "Too short!")
+      .required("An email is required"),
+  });
+
+  function handleSubmit(values: { email: string; password: string }) {
+    localStorage.setItem("", JSON.stringify(values));
+  }
+
   return (
-    <AuthForm
-      title="Welcome back!"
-      helperText="Log in to continue tracking your mood and sleep"
-      buttonText="Sign In"
-      linkText="Don't have an account?"
-      linkHref="/auth/login"
-      linkAction="Sign Up"
-    />
+    <>
+      <div className="bg-lightGradient h-screen flex flex-col gap-12 items-center justify-center">
+        <Icon name="logo" />
+        <div className="bg-white rounded-2xl p-6 shadow-md flex flex-col  justify-center min-h-[503px] w-full max-w-[530px] gap-3">
+          <Text className="text-4xl font-text-neutral-900 font-semibold">
+            {title}
+          </Text>
+          <Text className="text-neutral-300">{helperText}</Text>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={schema}
+            onSubmit={handleSubmit}
+          >
+            {(formikProps) => (
+              <form onSubmit={formikProps.handleSubmit}>
+                <div className="mb-4 flex flex-col text-neutral-800 font-semibold text-sm">
+                  <Label className="mb-2" htmlFor="email">
+                    Email address
+                  </Label>
+                  <TextInput
+                    placeholder="name@gmail.com"
+                    type="email"
+                    name="email"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col text-neutral-800 font-semibold text-sm">
+                  <Label className="mb-2" htmlFor="password">
+                    Password
+                  </Label>
+                  <TextInput type="password" name="password" />
+                </div>
+                <BasicButton style="w-full">{buttonText}</BasicButton>
+                <div className="text-center text-sm text-neutral-500 mt-8">
+                  <Text>
+                    {linkText}{" "}
+                    <a
+                      href={linkHref}
+                      className="text-blue-500 hover:underline"
+                    >
+                      {linkAction}
+                    </a>
+                  </Text>
+                </div>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </>
+    // <AuthForm
+    //   title="Welcome back!"
+    //   helperText="Log in to continue tracking your mood and sleep"
+    //   buttonText="Sign In"
+    //   linkText="Don't have an account?"
+    //   linkHref="/login"
+    //   linkAction="Sign Up"
+    // />
   );
 }
 
--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,15 +1,79 @@
-import AuthForm from "../../components/authform/AuthForm";
+import Icon from "../../components/icon/Icon";
+import { Formik } from "formik";
+import { Label, Text } from "react-aria-components";
+import BasicButton from "../../components/button/BasicButton";
+import TextInput from "../../components/input/TextInput";
+import * as Yup from "yup";
 
 function SignUp() {
+  const title = "Create an account";
+  const helperText = "Join to track your daily mood and sleep with ease.";
+  const buttonText = "Create an account";
+  const linkText = "Already got an account?";
+  const linkHref = "/login";
+  const linkAction = "Log in";
+
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email")
+      .min(2, "Too short!")
+      .required("An email is required"),
+  });
+
+  function handleSubmit(values: { email: string; password: string }) {
+    localStorage.setItem("", JSON.stringify(values));
+  }
+
   return (
-    <AuthForm
-      title="Create an account"
-      helperText="Join to track your daily mood and sleep with ease."
-      buttonText="Sign Up"
-      linkText="Already got an account?"
-      linkHref="/auth/signup"
-      linkAction="Log in."
-    />
+    <>
+      <div className="bg-lightGradient h-screen flex flex-col gap-12 items-center justify-center">
+        <Icon name="logo" />
+        <div className="bg-white rounded-2xl p-6 shadow-md flex flex-col  justify-center min-h-[503px] w-full max-w-[530px] gap-3">
+          <Text className="text-4xl font-text-neutral-900 font-semibold">
+            {title}
+          </Text>
+          <Text className="text-neutral-300">{helperText}</Text>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={schema}
+            onSubmit={handleSubmit}
+          >
+            {(formikProps) => (
+              <form onSubmit={formikProps.handleSubmit}>
+                <div className="mb-4 flex flex-col text-neutral-800 font-semibold text-sm">
+                  <Label className="mb-2" htmlFor="email">
+                    Email address
+                  </Label>
+                  <TextInput
+                    placeholder="name@gmail.com"
+                    type="email"
+                    name="email"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col text-neutral-800 font-semibold text-sm">
+                  <Label className="mb-2" htmlFor="password">
+                    Password
+                  </Label>
+                  <TextInput type="password" name="password" />
+                </div>
+                <BasicButton style="w-full">{buttonText}</BasicButton>
+                <div className="text-center text-sm text-neutral-500 mt-8">
+                  <Text>
+                    {linkText}{" "}
+                    <a
+                      href={linkHref}
+                      className="text-blue-500 hover:underline"
+                    >
+                      {linkAction}
+                    </a>
+                  </Text>
+                </div>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </>
   );
 }
 
