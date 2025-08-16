5e8a46c5ab2daebf4f26fd35bfe49074c1667012
Elyas <143660964+BlueElyas@users.noreply.github.com>
2025-08-16 17:22:24 +0100

    Update protected routes, simplifying them and removing state

--- src/components/layout/ProtectedRoutes.tsx
+++ src/components/layout/ProtectedRoutes.tsx
@@ -1,27 +1,14 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 function ProtectedRoutes() {
-  const [isChecking, setIsChecking] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [hasAccount, setHasAccount] = useState(false);
+  const user = localStorage.getItem("user");
+  const lasLoggedIn = localStorage.getItem("lastLoggedIn");
 
-  useEffect(() => {
-    const userString = localStorage.getItem("user");
-    const loggedIn = localStorage.getItem("lastLoggedIn");
-    if (!userString) return;
-
-    const userStringObj = JSON.parse(userString);
-
-    setHasAccount(Object.keys(userStringObj).length > 0);
-    setIsLoggedIn(loggedIn !== null);
-    setIsChecking(false);
-  }, []);
-
-  if (isChecking) return null;
-
-  if (isLoggedIn || hasAccount) return <Navigate to="/login" />;
-  if (!hasAccount) return <Navigate to="/create-account" />;
+  if (lasLoggedIn) {
+    return <Navigate to="/login" replace />;
+  } else if (!lasLoggedIn && !user) {
+    return <Navigate to="/create-account" replace />;
+  }
 
   return <Outlet />;
 }
